ed6caf55a7bf45b38c44cf941b8041201dba5dd5
Cheshire Cat <thacheshirecat@gmail.com>
2018-12-11 23:41:28 -0800

    Fix scaling and centering issue for ships, as well as impliment code to randomize which ships, without duplicates, appear on each track.

--- src/components/home/parallax/Parallax.tsx
+++ src/components/home/parallax/Parallax.tsx
@@ -17,20 +17,21 @@ import Gif12 from '../../../assets/images/ships/12.gif';
 
 function Parallax(){
   let background: number[] = AssetChoice(21);
-  let ships: number[] = AssetChoice(12);
-  let ship1: string = "Gif"+ships[0].toString();
-  console.log(Gif1);
+  let ships: any[] = ShipAssetChoice(11);
+  let ship1: any = ships[0];
+  let ship2: any = ships[1];
+  let ship3: any = ships[2];
   return(
     <div className="parallax-component">
       <div className="parallax-background">
         <span className="ships-scrolling">
-          <img className="ship" src={Gif5}/>
+          <img className="ship" src={ship1}/>
         </span>
         <span className="ships-scrolling">
-          <img className="ship" src={Gif3}/>
+          <img className="ship" src={ship2}/>
         </span>
         <span className="ships-scrolling">
-          <img className="ship" src={Gif10}/>
+          <img className="ship" src={ship3}/>
         </span>
       </div>
     </div>
@@ -38,17 +39,47 @@ function Parallax(){
 }
 export default Parallax;
 
-function AssetChoice(limit:number){
-  const assetMax: number = limit; 
-  let assetArray: number[] = [];
-  while(assetArray.length < 4){
+function ShipAssetChoice(limit:number){
+  const assetMax: number = limit;
+  const assetList: any[] = [Gif1, Gif2, Gif3, Gif4, Gif5, Gif6, Gif7, Gif8, Gif9, Gif10, Gif11, Gif12];
+  let assetArray: any[] = [];
+  while(assetArray.length < 3){
     let newNumber: number = Math.round(Math.random()*assetMax);
-    if(newNumber < assetMax && newNumber != 0){
-      assetArray.push(newNumber);
-    }else{
-      assetArray.push(1);
+    let invalidCounter = 0;
+    for(let x = 0; x < assetArray.length; x++){
+      if(assetArray[x].toString() === assetList[newNumber].toString()){
+        invalidCounter++;
+      }
+    }
+    if(assetArray.length != 0 && invalidCounter === 0){
+      assetArray.push(assetList[newNumber]);
+    } else if (assetArray.length === 0){
+      assetArray.push(assetList[newNumber]);
     }
   }
+  return assetArray;
+}
+
+function AssetChoice(limit:number){
+  const assetMax: number = limit;
+  const assetList: any[] = [Gif1, Gif2, Gif3, Gif4, Gif5, Gif6, Gif7, Gif8, Gif9, Gif10, Gif11, Gif12];
+  let assetArray: any[] = [];
+  // while(assetArray.length < 3){
+  //   let newNumber: number = Math.round(Math.random()*assetMax);
+  //   for(let x = 0; x < assetArray.length; x++)
+  //   {
+  //     if(assetArray[x] != assetList[newNumber])
+  //     {
+  //       console.log("Asset array at possition: " + x + " = " + assetArray[x]);
+  //       console.log("Asset list at possition: " + newNumber + " = " + assetList[newNumber]);
+  //       assetArray.push(assetList[newNumber]);
+  //     }
+  //   }
+  //   if(assetArray.length === 0)
+  //   {
+  //     assetArray.push(assetList[newNumber]);
+  //   }
+  // }
   return assetArray;
 }
 
