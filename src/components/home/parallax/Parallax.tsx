55d9a2be394bbf20eeae4b4e4e6019fa08100036
hanleyjames <hanley.doth@outlook.com>
2018-12-12 19:31:54 -0800

    finish random bg function

--- src/components/home/parallax/Parallax.tsx
+++ src/components/home/parallax/Parallax.tsx
@@ -35,14 +35,15 @@ import Gif11 from '../../../assets/images/ships/11.gif';
 import Gif12 from '../../../assets/images/ships/12.gif';
 
 function Parallax(){
-  let background: number[] = AssetChoice(21);
+  let background: any[] = AssetChoice(20);
   let ships: any[] = ShipAssetChoice(11);
   let ship1: any = ships[0];
   let ship2: any = ships[1];
   let ship3: any = ships[2];
+  let bgchoice: any = background[0];
   return(
     <div className="parallax-component">
-      <div className="parallax-background">
+      <div className="parallax-background" >
         <span className="ships-scrolling">
           <img className="ship" src={ship1}/>
         </span>
@@ -81,42 +82,9 @@ function ShipAssetChoice(limit:number){
 
 function AssetChoice(limit:number){
   const assetMax: number = limit;
-  const assetList: any[] = [Gif1, Gif2, Gif3, Gif4, Gif5, Gif6, Gif7, Gif8, Gif9, Gif10, Gif11, Gif12];
+  const assetList: any[] = [BG1, BG2, BG3, BG4, BG5, BG6, BG7, BG8, BG9, BG10, BG11, BG12, BG13, BG14, BG15, BG16, BG17, BG18, BG19, BG20, BG21];
   let assetArray: any[] = [];
-  // while(assetArray.length < 3){
-  //   let newNumber: number = Math.round(Math.random()*assetMax);
-  //   for(let x = 0; x < assetArray.length; x++)
-  //   {
-  //     if(assetArray[x] != assetList[newNumber])
-  //     {
-  //       console.log("Asset array at possition: " + x + " = " + assetArray[x]);
-  //       console.log("Asset list at possition: " + newNumber + " = " + assetList[newNumber]);
-  //       assetArray.push(assetList[newNumber]);
-  //     }
-  //   }
-  //   if(assetArray.length === 0)
-  //   {
-  //     assetArray.push(assetList[newNumber]);
-  //   }
-  // }
+  let newNumber: number = Math.round(Math.random()*assetMax);
+  assetArray.push(assetList[newNumber]);
   return assetArray;
 }
-
-
-// This section needs to be redone. Currently it will not load any changes for img's, saying it is an invalid format.
-// React.createElement('div', { className: 'parallax-component' },
-//   React.createElement('div', { className: 'parallax-background'},
-//   [
-//     React.createElement('marquee',{ behavior: 'scroll', direction: 'right', scrollamount: Math.round(Math.random()*15)},
-//       <img className="ship" src={Gif5} />),
-//     React.createElement('marquee', { behavior: 'scroll', direction: 'right', scrollamount: Math.round(Math.random()*15)},
-//       <img className="ship" src={Gif12} />),
-//     React.createElement('marquee', { behavior: 'scroll', direction: 'right', scrollamount: Math.round(Math.random()*15)},
-//       <img className="ship" src={Gif1} />)
-//   ]
-// )
-// )
-
-// function SpeedLimit(){
-//   const
-// }
