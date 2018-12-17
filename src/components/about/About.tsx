b56f3771e0452667cf57b84265f9a6301e8f71a3
hanleyjames <hanley.doth@outlook.com>
2018-12-17 12:49:56 -0800

    Add portfolio Shots for everyone but James, basic default content for placeholder titles and name

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -5,6 +5,34 @@ class About extends React.Component {
   render(){
     return (
       <div className="About">
+        <div className="about-header">
+        </div>
+        <div className="contributors">
+          <div className="contributor">
+            <h3 className="name">Jade</h3>
+            <h4 className="title">C#/Unity Developer</h4>
+          </div>
+          <div className="contributor">
+            <h3 className="name">Brad</h3>
+            <h4 className="title">UX/UI Artist</h4>
+          </div>
+          <div className="contributor">
+            <h3 className="name">James</h3>
+            <h4 className="title">C#/Javascript Engineer</h4>
+          </div>
+          <div className="contributor">
+            <h3 className="name">Alex</h3>
+            <h4 className="title">Javascript Engineer</h4>
+          </div>
+          <div className="contributor">
+            <h3 className="name">Brian</h3>
+            <h4 className="title">Javascript Engineer</h4>
+          </div>
+          <div className="contributor">
+            <h3 className="name">Derek</h3>
+            <h4 className="title">Javascript Engineer</h4>
+          </div>
+        </div>
       </div>
     );
   }
