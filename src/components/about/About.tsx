03d14f8f5b58d362e248baa773af481e07f2b1ec
hanleyjames <hanley.doth@outlook.com>
2018-12-17 13:15:23 -0800

    Added images and titles to about page

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import './About.css';
+import Alex from '../../assets/images/portfolio/Alex.jpeg';
+import Brad from '../../assets/images/portfolio/Brad.png';
+import Brian from '../../assets/images/portfolio/Brian.jpeg';
+import Derek from '../../assets/images/portfolio/Derek.jpeg';
+
 
 class About extends React.Component {
   render(){
@@ -14,7 +19,12 @@ class About extends React.Component {
           </div>
           <div className="contributor">
             <h3 className="name">Brad</h3>
+            <img src={Brad} />
             <h4 className="title">UX/UI Artist</h4>
+          </div>
+          <div className="contributor">
+            <h3 className="name">Justin</h3>
+            <h4 className="title">Marketing Campaign Manager</h4>
           </div>
           <div className="contributor">
             <h3 className="name">James</h3>
@@ -22,14 +32,17 @@ class About extends React.Component {
           </div>
           <div className="contributor">
             <h3 className="name">Alex</h3>
+            <img src={Alex} />
             <h4 className="title">Javascript Engineer</h4>
           </div>
           <div className="contributor">
             <h3 className="name">Brian</h3>
+            <img src={Brian} />
             <h4 className="title">Javascript Engineer</h4>
           </div>
           <div className="contributor">
             <h3 className="name">Derek</h3>
+            <img src={Derek} />
             <h4 className="title">Javascript Engineer</h4>
           </div>
         </div>
